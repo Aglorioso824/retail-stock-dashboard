5aafbeb10c438b90c454ff143c4a548b387fd08b
Aglorioso824 <203539980+Aglorioso824@users.noreply.github.com>
2025-04-28 14:11:54 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -59,7 +59,26 @@ SKU_MAPPING = {
     "226-802610001": "Solo",
     "226-802620001": "Solo & Printer",
     "226-902600701": "3G",
-    # ... (other mappings) ...
+    "386803    :  SP6 SP6 POS L &SOLO": "POS Lite",
+    "537815    :  SP6 SP6 SUMUP  SOLO": "Solo",
+    "604611    :  SP6 SUMUP AIR": "Air",
+    "660513    :  SP6 AIR BUNDL E": "Air Bundle",
+    "626938    :  SP6 SUMUPSOLL OPRNTER": "Solo & Printer",
+    "613971    :  SP6 SP6 SUMUP  3G+ PK": "3G PK",
+    "SUMUP AIR CRADLE BUNDLE PK1": "Air Bundle",
+    "SUMUP SOLO                PK1": "Solo",
+    "SUMUP 3G PAYMENT KIT/PRINTER PK1 DNO": "3G PK",
+    "DX SUMUP AIR CARD PAYMENT DEVICE PK1": "Air",
+    "DX SUMUP 3G CARD PAYMENT DEVICE PK1 DNO": "3G",
+    "3593051055-SumUp Air Card Reader-A Smarter Way to Get Paid": "Air",
+    "3597012311-SumUp Solo Smart Card Terminal-": "Solo",
+    "3597012312-SumUp AirPlus Cradle Bundle-": "Air Bundle",
+    "3597012314-SumUp POS Lite Solo Bundle-": "POS Lite",
+    "1250000000-SumUp Solo+ Printer-Payment Card Reader": "Solo & Printer",
+    "SUMUP AIR CARD READER EACH": "Air",
+    "SUMUP SOLO SMART CARD TERMINAL EACH": "Solo",
+    "SOLO & PRINTER BUNDLE RETAIL UK EACH": "Solo & Printer",
+    "POS LITE & SOLO BUNDLE UK EACH": "POS Lite",
 }
 
 # ------------------------------------------------------------------------
@@ -67,7 +86,18 @@ SKU_MAPPING = {
 # ------------------------------------------------------------------------
 IGNORED_SKUS = [
     "SUMUP 3G PAYMENT KIT/PRINTER PK1 DNO",
-    # ... (other SKUs) ...
+    "DX SUMUP 3G CARD PAYMENT DEVICE PK1 DNO",
+    "613971    :  SP6 SP6 SUMUP  3G+ PK",
+    "226-902600701",
+    "3597012300-SumUp Air Cradle-Docking Station White",
+    "3597012310-SumUp Air Reader and Cradle-Bundle White",
+    "3597016543-SumUp 3G + Wifi Payment Reader-Standalone Card White",
+    "3597016544-SumUp 3G Payment Kit-",
+    "3597012301-Solo SumUp Card Reader-",
+    "3597012313-Sumup Point of Sale Lite-",
+    "SUMUP AIR CHARGING CRADLE CHARGER EACH",
+    "SUMUP 3G+ WIFI CARD READER PAYMENT KIT EACH",
+    "SUMUP 3G+ WIFI CARD READER EACH",
 ]
 
 # ------------------------------------------------------------------------
